063507adc89942e4e1e56492bdf2eb0685e8f402
Olaf Kryus <14991562+olafkryus@users.noreply.github.com>
2021-04-28 22:37:01 +0200

    Add endianness test
    (cherry picked from commit aceb6d844882d6e98fdcca15db527728452636b9)

--- tests/DataType/BinaryValueTest.php
+++ tests/DataType/BinaryValueTest.php
@@ -19,11 +19,36 @@ class BinaryValueTest extends TestCase
         self::assertSame($signedness, $value->isSigned());
     }
 
+    /**
+     * @dataProvider endiannessProvider
+     */
+    public function testDoesReturnCorrectEndianness(int $endianness): void
+    {
+        $value = new BinaryValue(' ', $endianness);
+
+        self::assertSame($endianness, $value->getEndianness());
+    }
+
+    public function testCannotInstantiateWithInvalidEndianness(): void
+    {
+        $this->expectException(\Exception::class);
+
+        new BinaryValue(' ', 2);
+    }
+
     public function signednessProvider(): array
     {
         return [
             'signed' => [true],
             'unsigned' => [false],
+        ];
+    }
+
+    public function endiannessProvider(): array
+    {
+        return [
+            'little endian' => [Endianness::ENDIANNESS_LITTLE_ENDIAN],
+            'big endian' => [Endianness::ENDIANNESS_BIG_ENDIAN],
         ];
     }
 }
