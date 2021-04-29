a3321a5d4110781e27fcc95074c4ddc57cf0b387
Olaf Kryus <14991562+olafkryus@users.noreply.github.com>
2021-04-29 23:19:34 +0200

    Refactor int value test

--- tests/DataType/BinaryValueTest.php
+++ tests/DataType/BinaryValueTest.php
@@ -39,14 +39,8 @@ class BinaryValueTest extends TestCase
     /**
      * @dataProvider intValueTestProvider
      */
-    public function testDoesReturnCorrectIntValue(
-        string $value,
-        int $endianness,
-        bool $signed,
-        int $expectedValue
-    ): void {
-        $value = new BinaryValue($value, $endianness, $signed);
-
+    public function testDoesReturnCorrectIntValue(BinaryValue $value, int $expectedValue): void
+    {
         self::assertSame($expectedValue, $value->toInt());
     }
 
@@ -66,25 +60,53 @@ class BinaryValueTest extends TestCase
         ];
     }
 
-    public function intValueTestProvider(): array
+    private function getValueTestSet(): array
     {
         $x80 = chr(0x80);
         $x8081 = chr(0x80) . chr(0x81);
         $x00008081 = chr(0x00) . chr(0x00) . chr(0x80) . chr(0x81);
 
         return [
-            '1-byte little endian signed' => [$x80, Endianness::ENDIANNESS_LITTLE_ENDIAN, true, -0x7F - 1],
-            '1-byte big endian signed' => [$x80, Endianness::ENDIANNESS_BIG_ENDIAN, true, -0x7F - 1],
-            '1-byte little endian unsigned' => [$x80, Endianness::ENDIANNESS_LITTLE_ENDIAN, false, 0x80],
-            '1-byte big endian unsigned' => [$x80, Endianness::ENDIANNESS_BIG_ENDIAN, false, 0x80],
-            '2-byte little endian signed' => [$x8081, Endianness::ENDIANNESS_LITTLE_ENDIAN, true, -0x7E7F - 1],
-            '2-byte big endian signed' => [$x8081, Endianness::ENDIANNESS_BIG_ENDIAN, true, -0x7F7E - 1],
-            '2-byte little endian unsigned' => [$x8081, Endianness::ENDIANNESS_LITTLE_ENDIAN, false, 0x8180],
-            '2-byte big endian unsigned' => [$x8081, Endianness::ENDIANNESS_BIG_ENDIAN, false, 0x8081],
-            '4-byte little endian signed' => [$x00008081, Endianness::ENDIANNESS_LITTLE_ENDIAN, true, -0x7E7FFFFF - 1],
-            '4-byte big endian signed' => [$x00008081, Endianness::ENDIANNESS_BIG_ENDIAN, true, 0x00008081],
-            '4-byte little endian unsigned' => [$x00008081, Endianness::ENDIANNESS_LITTLE_ENDIAN, false, 0x81800000],
-            '4-byte big endian unsigned' => [$x00008081, Endianness::ENDIANNESS_BIG_ENDIAN, false, 0x00008081],
+            '1-byte little endian signed' => new BinaryValue($x80, Endianness::ENDIANNESS_LITTLE_ENDIAN, true),
+            '1-byte big endian signed' => new BinaryValue($x80, Endianness::ENDIANNESS_BIG_ENDIAN, true),
+            '1-byte little endian unsigned' => new BinaryValue($x80, Endianness::ENDIANNESS_LITTLE_ENDIAN, false),
+            '1-byte big endian unsigned' => new BinaryValue($x80, Endianness::ENDIANNESS_BIG_ENDIAN, false),
+            '2-byte little endian signed' => new BinaryValue($x8081, Endianness::ENDIANNESS_LITTLE_ENDIAN, true),
+            '2-byte big endian signed' => new BinaryValue($x8081, Endianness::ENDIANNESS_BIG_ENDIAN, true),
+            '2-byte little endian unsigned' => new BinaryValue($x8081, Endianness::ENDIANNESS_LITTLE_ENDIAN, false),
+            '2-byte big endian unsigned' => new BinaryValue($x8081, Endianness::ENDIANNESS_BIG_ENDIAN, false),
+            '4-byte little endian signed' => new BinaryValue($x00008081, Endianness::ENDIANNESS_LITTLE_ENDIAN, true),
+            '4-byte big endian signed' => new BinaryValue($x00008081, Endianness::ENDIANNESS_BIG_ENDIAN, true),
+            '4-byte little endian unsigned' => new BinaryValue($x00008081, Endianness::ENDIANNESS_LITTLE_ENDIAN, false),
+            '4-byte big endian unsigned' => new BinaryValue($x00008081, Endianness::ENDIANNESS_BIG_ENDIAN, false),
         ];
+    }
+
+    public function intValueTestProvider(): array
+    {
+        $values = $this->getValueTestSet();
+
+        $expectedValues = [
+            '1-byte little endian signed' => -0x7F - 1,
+            '1-byte big endian signed' => -0x7F - 1,
+            '1-byte little endian unsigned' => 0x80,
+            '1-byte big endian unsigned' => 0x80,
+            '2-byte little endian signed' => -0x7E7F - 1,
+            '2-byte big endian signed' => -0x7F7E - 1,
+            '2-byte little endian unsigned' => 0x8180,
+            '2-byte big endian unsigned' => 0x8081,
+            '4-byte little endian signed' => -0x7E7FFFFF - 1,
+            '4-byte big endian signed' => 0x00008081,
+            '4-byte little endian unsigned' => 0x81800000,
+            '4-byte big endian unsigned' => 0x00008081,
+        ];
+
+        return array_map(
+            static function ($value, $key) use ($expectedValues) {
+                return [$value, $expectedValues[$key]];
+            },
+            $values,
+            array_keys($values)
+        );
     }
 }
